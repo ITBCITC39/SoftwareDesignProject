02afeb4ea34cd39af0b296d25b37d198644cba09
ITBCITC39 <dbobak@trincoll.edu>
2024-10-18 16:15:06 -0400

    Update Employee.kt

--- src/main/kotlin/edu/trincoll/hr/Employee.kt
+++ src/main/kotlin/edu/trincoll/hr/Employee.kt
@@ -1,16 +1,20 @@
 package edu.trincoll.hr
 
-// Abstract class Employee should have:
-//   - a name of type String
-//   - an id of type Int.
-// It should implement the Comparable interface with the
-// compareTo method.
-//
-// It should include an abstract method pay()
-// that returns a Double.
-//
-// It should override the toString method to
-// return a string with the name and id of the employee.
 abstract class Employee(
-) {
+    val name: String,
+    val id: Int
+) : Comparable<Employee> {
+
+    // Abstract method for pay
+    abstract fun pay(): Double
+
+    // Implementing compareTo method
+    override fun compareTo(other: Employee): Int {
+        return this.id - other.id
+    }
+
+    // Overriding toString method
+    override fun toString(): String {
+        return "Employee(name='$name', id=$id)"
+    }
 }
