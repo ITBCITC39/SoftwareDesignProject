fa1d3cefcfc3701ee80913f545c5b4ad77b713f8
ITBCITC39 <dbobak@trincoll.edu>
2024-10-24 11:32:43 -0400

    Update Salaried.kt

--- src/main/kotlin/edu/trincoll/hr/Salaried.kt
+++ src/main/kotlin/edu/trincoll/hr/Salaried.kt
@@ -1,17 +1,12 @@
 package edu.trincoll.hr
 
-
-class Salaried(
-    name: String,
-    id: Int,
-    private val salary: Double
-) : Employee(name, id) {
+class Salaried(name: String, id: Int, val salary: Double) : Employee(name, id) {
 
     override fun pay(): Double {
         return salary / 26.0
     }
 
     override fun toString(): String {
-        return "Salaried(name='$name', id=$id, salary=$salary)"
+        return "Salaried(name=$name, id=$id, salary=$salary)"
     }
 }
